8a227c352a78f5a0c0cee9c5c9a73d85e0015598
Ichraq <146119083+Ichraq05@users.noreply.github.com>
2024-02-03 17:24:50 +0000

    ---

--- Structure_de_données/TME_1-2/Ex1/tme1_exo1p1.c
+++ Structure_de_données/TME_1-2/Ex1/tme1_exo1p1.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//gcc -ggdb -o tme1_exo1p1 tme1_exo1p1.c
+//gdb tme1_exo1p1
+
+//gcc -o tme1_exo1p1 tme1_exo1p1.c
+
 const static int len = 10;
 
 int main(void) {
     int *tab;
     unsigned int i;
 
+    //printf("[");
+
     tab = (int*)malloc(len*sizeof(int));
 
     for (i=len-1; i>=0; i--) {
         tab[i] = i;
+        //printf("%d, ", tab[i]);
     }
+
+    //printf("%d]\n", tab[0]);
 
     free(tab);
     return 0;
